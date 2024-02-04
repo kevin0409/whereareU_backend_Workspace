c5e9bb6e4bcf40f91e6842c5562620a5db5ede84
박승호 <joinus0409@naver.com>
2024-02-04 16:00:02 +0900

    다중 쓰레드로 변경

--- api/run.py
+++ api/run.py
@@ -1,8 +1,9 @@
 from app import create_app
-import os
+from werkzeug.serving import run_simple
 
 app = create_app()
 
 if __name__ == '__main__':
 
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    #app.run(host='127.0.0.1', port=5000, debug=True)
+    run_simple('127.0.0.1', 5000, app, threaded=True)
