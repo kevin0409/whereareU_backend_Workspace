463a45a925c750d33662949002f44f9060f4cdb0
박승호 <joinus0409@naver.com>
2024-02-08 17:53:09 +0900

    다중 쓰레드 변경

--- api/run.py
+++ api/run.py
@@ -4,6 +4,5 @@ from werkzeug.serving import run_simple
 app = create_app()
 
 if __name__ == '__main__':
-
-    #app.run(host='127.0.0.1', port=5000, debug=True)
+    
     run_simple('127.0.0.1', 5000, app, threaded=True)
